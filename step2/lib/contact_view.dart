a81f8c13f29d2e21fcd533542bdf22b45fc562c6
B R <br@rgroup2003.com>
2017-07-21 19:03:43 -0400

    Updated for latest flutter release
    ListItem and MaterialList were either depreciated or doesn't exist anymore and were replaced with ListTile and ListView. The current code is compatible with the current release of flutter.

--- step2/lib/contact_view.dart
+++ step2/lib/contact_view.dart
@@ -16,39 +16,33 @@ class ContactsPage extends StatelessWidget {
 }
 
 
-class ContactList extends StatelessWidget {
+class ContactList extends StatelessWidget{
 
   final List<Contact> _contacts;
 
   ContactList(this._contacts);
 
   @override
-  Widget build(BuildContext context) {
-    return new MaterialList(
-          type: MaterialListType.twoLine,
-          padding: new EdgeInsets.symmetric(vertical: 8.0),
-          children: _buildContactList()
-        );
+  Widget build(BuildContext context){
+    return new ListView(
+      padding: new EdgeInsets.symmetric(vertical: 8.0),
+      children: _buildContactList()
+    );
   }
-
-  List<_ContactListItem> _buildContactList() {
-    return _contacts.map((contact) => new _ContactListItem(contact))
-                    .toList();
+  List<_ContactListItem> _buildContactList(){
+    return _contacts.map((contact) => new _ContactListItem(contact)).toList();
   }
-
 }
 
 
+class _ContactListItem extends ListTile{
 
-class _ContactListItem extends ListItem {
-
-  _ContactListItem(Contact contact) :
-    super(
-      title : new Text(contact.fullName),
-      subtitle: new Text(contact.email),
-      leading: new CircleAvatar(
-        child: new Text(contact.fullName[0])
-      )
-    );
-
+  _ContactListItem(Contact contact):
+  super(
+    title: new Text(contact.fullName),
+    subtitle: new Text(contact.email),
+    leading: new CircleAvatar(
+      child: new Text(contact.fullName[0])
+    )
+  );
 }
